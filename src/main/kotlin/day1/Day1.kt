491963fbf2b339a5ea12511e0742356c31e264b9
DUCHESNE Alexandre <alexandre.duchesne@telecomnancy.eu>
2022-12-02 10:25:39 +0000

    Update Day1.kt

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -2,15 +2,6 @@ package day1
 
 import java.io.File
 
-operator fun String.times(n: Int): String {
-    val sb = StringBuilder()
-    repeat(n) {
-        sb.append(this)
-    }
-    return sb.toString()
-}
-
-
 fun main() {
     val lines = File("src/main/resources/day1").readLines().joinToString("\n")
     println(lines.split("\n\n").map { it -> it.split("\n").sumOf { it.toInt() } }.sorted().takeLast(1).sum())
