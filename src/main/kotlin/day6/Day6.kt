84c6ec5dea088c8f4fd53c3fd19b27fc5cafce27
LimpSword <contact@limpsword.fr>
2022-12-07 11:26:34 +0100

    better day6 code

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -4,34 +4,19 @@ import days.Day
 import java.io.File
 
 class Day6 : Day(6) {
-    override fun parsing() {
 
+    private lateinit var text: String
+
+    override fun parsing() {
+        text = File("src/main/resources/day6").readText()
     }
 
     override fun partOne(): Any {
-        val lines = File("src/main/resources/day6").readText()
-
-        var counter = 0
-        for (i in 0..lines.length - 3) {
-            if (lines.substring(i, i + 4).toCharArray().toSet().size == 4) {
-                break
-            }
-            counter++
-        }
-        return counter + 4
+        return text.windowed(4).indexOfFirst { it.toCharArray().distinct().size == 4 }
     }
 
     override fun partTwo(): Int {
-        val lines = File("src/main/resources/day6").readText()
-
-        var counter = 0
-        for (i in 0..lines.length - 13) {
-            if (lines.substring(i, i + 14).toCharArray().toSet().size == 14) {
-                break
-            }
-            counter++
-        }
-        return counter + 14
+        return text.windowed(14).indexOfFirst { it.toCharArray().distinct().size == 14 }
     }
 
 }
