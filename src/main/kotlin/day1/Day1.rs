499207b0f02c1ce6549c3586a02173f3c7d2d65c
LimpSword <contact@limpsword.fr>
2022-12-08 16:30:32 +0100

    reformatted file

--- src/main/kotlin/day1/Day1.rs
+++ src/main/kotlin/day1/Day1.rs
@@ -2,9 +2,25 @@ use std::collections::LinkedList;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::ptr::{null, null_mut};
 use itertools::Itertools;
+use std::time::{Duration, Instant};
 
 fn main() {
+    let list = parse();
+    let start1 = Instant::now();
+    part1(&list);
+    let duration1 = start1.elapsed();
+
+    let start2 = Instant::now();
+    part2(&list);
+    let duration2 = start2.elapsed();
+
+    println!("[PART 1] Time elapsed: {:?}", duration1);
+    println!("[PART 2] Time elapsed: {:?}", duration2);
+}
+
+fn parse() -> LinkedList<LinkedList<i32>> {
     if let Ok(lines) = read_lines("src/main/resources/day1") {
         let mut list = LinkedList::new();
         let mut current_list: LinkedList<i32> = LinkedList::new();
@@ -18,10 +34,19 @@ fn main() {
                 }
             }
         }
-
-        let totals: _ = list.into_iter().map(|x| x.iter().sum::<i32>()).collect::<LinkedList<_>>();
-        println!("{}", totals.iter().sorted().rev().take(3).sum::<i32>());
+        return list;
     }
+    return LinkedList::new();
+}
+
+fn part1(list: &LinkedList<LinkedList<i32>>) {
+    let totals: _ = list.into_iter().map(|x| x.iter().sum::<i32>()).collect::<LinkedList<_>>();
+    println!("{}", totals.iter().sorted().rev().take(3).sum::<i32>());
+}
+
+fn part2(list: &LinkedList<LinkedList<i32>>) {
+    let totals: _ = list.into_iter().map(|x| x.iter().sum::<i32>()).collect::<LinkedList<_>>();
+    println!("{}", totals.iter().sorted().rev().take(1).sum::<i32>());
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
